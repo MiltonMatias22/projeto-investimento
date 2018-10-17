48ad9971611c75554425f752d742f7ae04b653fb
MiltonMatias22 <nodout.develops@gmail.com>
2018-10-17 10:05:39 -0300

    Moviment: model & migrate

---
+++ app/Entities/Moviment.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+/**
+ * Class Moviment.
+ *
+ * @package namespace App\Entities;
+ */
+class Moviment extends Model implements Transformable
+{
+    use TransformableTrait;
+
+    use SoftDeletes;
+    use Notifiable;
+
+    public $timestamps = true;
+
+    protected $table   = 'moviments';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'group_id',
+        'product_id',
+        'value',
+        'type'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+    
+    public function product()
+    {
+        return $this->belongsTo(Produto::class);
+    }
+
+}
